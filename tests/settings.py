2e4b4e041f0061b03a7f99c14ce31e3f6a59f1d9
Stephan Jaekel <steph@rdev.info>
2018-02-23 15:27:27 +0100

    Add test basics.

---
+++ tests/settings.py
@@ -0,0 +1,26 @@
+import os
+
+
+DEBUG = True
+
+SECRET_KEY = 'testing'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+INSTALLED_APPS = (
+    'tapeforms',
+)
+
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [os.path.join(os.path.dirname(__file__), 'templates')],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'debug': True
+    }
+}]
