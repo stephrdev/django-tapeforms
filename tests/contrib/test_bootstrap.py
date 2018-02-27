78e66f775c5d8b912e3244cfbc89400ac229fb33
Stephan Jaekel <steph@rdev.info>
2018-02-27 14:36:12 +0100

    Add tests for bootstrap contrib mixin.

---
+++ tests/contrib/test_bootstrap.py
@@ -0,0 +1,45 @@
+from django import forms
+
+from tapeforms.contrib.bootstrap import BootstrapTapeformMixin
+
+
+class DummyForm(BootstrapTapeformMixin, forms.Form):
+    my_field1 = forms.CharField()
+    my_field2 = forms.BooleanField()
+
+
+class TestBootstrapTapeformMixin:
+
+    def test_field_template(self):
+        form = DummyForm()
+        assert form.field_template == 'tapeforms/fields/bootstrap.html'
+
+    def test_field_container_css_class_default(self):
+        form = DummyForm()
+        assert form.get_field_container_css_class(
+            form['my_field1']) == 'form-group'
+
+    def test_field_container_css_class_checkbox(self):
+        form = DummyForm()
+        assert form.get_field_container_css_class(
+            form['my_field2']) == 'form-check'
+
+    def test_field_label_css_class_default(self):
+        form = DummyForm()
+        assert form.get_field_label_css_class(
+            form['my_field1']) is None
+
+    def test_field_label_css_class_checkbox(self):
+        form = DummyForm()
+        assert form.get_field_label_css_class(
+            form['my_field2']) == 'form-check-label'
+
+    def test_widget_css_class_default(self):
+        form = DummyForm()
+        assert form.get_widget_css_class(
+            'my_field1', form.fields['my_field1']) == 'form-control'
+
+    def test_widget_css_class_checkbox(self):
+        form = DummyForm()
+        assert form.get_widget_css_class(
+            'my_field2', form.fields['my_field2']) == 'form-check-input'
