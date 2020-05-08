246ccce735b5e2280fc5e27b30f64bf916958177
Jérôme Lebleu <jeromelebleu@users.noreply.github.com>
2020-05-08 20:35:11 +0200

    Add 3.7 & 3.8 to tested Python versions

--- setup.py
+++ setup.py
@@ -36,5 +36,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
